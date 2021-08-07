2b1d126bb1f7ac4be375b8ef5e5fbba6cd0cfd78
shahnawazkhan988 <shahnawazkhan988@gmail.com>
2021-08-07 16:11:53 +0200

    first commit

---
+++ src/test/java/com/example/attsk/AttskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attsk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
